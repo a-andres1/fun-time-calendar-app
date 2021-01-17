383a52fd9c80da9fd0bc6e8f1baf42c0cf741392
alyssa andres <alyssaandres1@gmail.com>
2021-01-17 18:52:17 -0500

    created loop to create hourDivs

--- script.js
+++ script.js
@@ -9,29 +9,37 @@ var printDate = currentDate.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
 topDate.text(printDate);
 
 
+// array for mainHourDiv to loop through
+var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm",]
+
+// for loop that creates divs for each hour
+for (var i = 0; i < hours.length; i++){
 // create hour divs 
 var mainHourDiv = $("<div>");
 // adds class - row
 mainHourDiv.addClass("row");
 // apppends div to container
 $(".container").append(mainHourDiv);
+}
 
 // creates column for holding hour times
 var hourColumn = $("<div>");
 // adds column class for spacing
 hourColumn.addClass("col-md-1 hour");
 // appends coloumn to mainHourDiv row
-$(".row").append(hourColumn);
+$(".row").prepend(hourColumn);
 // creates text in hourColumn
-hourColumn.text("hours")
+for (var i = 0; i < hours.length; i++){
+hourColumn.text(hours[i]);
+}
 
-var textArea = $("<div>");
+var textArea = $("<input>");
 // adds classes for spacing to textArea divs
-textArea.addClass("col-md-10 textFill timeblock");
+textArea.addClass("col-md-10 textFill input-group timeblock");
 // appends the text area to the mainHourDiv row
 $(".row").append(textArea);
 // adds text to see the spacing for now
-textArea.text("text goes here")
+// $("div.timeblock").text("text goes here")
 
 // creates save button 
 var saveBtn = $("<button>");
@@ -40,11 +48,6 @@ saveBtn.addClass("col-md-1 saveBtn");
 // appends button to mainHourDiv
 $(".row").append(saveBtn);
 // displays text to test if it's working
-saveBtn.text("save")
+$("button.saveBtn").text("save")
 
-// array for mainHourDiv to loop through
-var hours = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6",]
 
-for (var i = 0; i<hours.length; i++){
-
-}
