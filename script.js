d983d389d145c75c3aa2997f87104a17fbcfa403
xena <74474948+a-andres1@users.noreply.github.com>
2021-01-23 11:53:33 -0500

    refactored script file for brevity, added floats to css to structure page correctly, was able to get id's to populate correctly finally!

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ $().ready(function () {
 // luxon date 
 var currentDate = luxon.DateTime.local();
 var topDate = $("#currentDay");
-var printDate = currentDate.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
+var printDate = currentDate.toLocaleString(luxon.DateTime.DATETIME_FULL);
 topDate.text(printDate);
+// gets current hour
+console.log(luxon.DateTime.local().c.hour);
 
 
 // array for mainHourDiv to loop through
@@ -14,56 +16,43 @@ var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "
 
 // for loop that creates divs for each hour
 for (var i = 0; i < hours.length; i++){
-// create hour divs 
-var mainHourDiv = $("<div>");
-// adds class - row
-mainHourDiv.addClass("row").attr("id", hours[i]);
-
-// creates column for holding hour times
-var hourColumn = $("<div>" + hours[i] + "</div>");
-// adds column class for spacing
-hourColumn.addClass("col-md-1 hour");
-
-
-var textArea = $("<input>");
-// adds classes for spacing to textArea divs
-textArea.addClass("col-md-10 textFill input-group timeblock");
-
-// creates save button 
-var saveBtn = $("<button>");
-// adds classes to button
-saveBtn.addClass("col-md-1 saveBtn");
+// create hour divs with class and ids
+var mainHourDiv = $("<div>").addClass("row");
+// creates column for holding hour times with column class for spacing
+var hourColumn = $("<div>").addClass("col-md-1 hour").attr("id", hours[i]).text(hours[i]);
+// creates input area with classes for spacing to textArea divs
+var textArea = $("<input>").addClass("col-md-10 textFill input-group timeblock inputGroup-sizing-lg");
+// creates save button with classes
+var saveBtn = $("<button>").addClass("col-md-1 saveBtn");
 
 // apppends div to container
-$(".container").append(mainHourDiv);
+$(".container").append(mainHourDiv); 
+// appends coloumn to mainHourDiv row
+$(".container").append(hourColumn);   
+// appends the text area to the mainHourDiv row
+$(".container").append(textArea);
+// appends button to mainHourDiv
+$(".container").append(saveBtn);
+
 }
 
-
-// appends coloumn to mainHourDiv row
-$(".row").append(hourColumn);
-// creates text in hourColumn
-// for (var i = 0; i < hours.length; i++){
-// $("div.hour").attr("id", hours[i]);
-// console.log (hours[i]);
-// }
-
-
-// appends the text area to the mainHourDiv row
-$(".row").append(textArea);
-// adds text to see the spacing for now
-// $("div.timeblock").text("text goes here")var 
-
-
-
-// appends button to mainHourDiv
-$(".row").append(saveBtn);
 // displays text to test if it's working
-$("button.saveBtn").text("save")
-
+$("button.saveBtn").text("save");
 
 $(".saveBtn").click(function(){
+    $(this)
 console.log("save");
 var savedText = $("#textFill").val();
 
 localStorage.setItem("save", savedText)
+
+// getItem
 });
+
+
+// function (checkTime){
+// check the text? in hourColumn against the hour in Luxon to assign past present or future
+// if 
+    
+
+// } 
